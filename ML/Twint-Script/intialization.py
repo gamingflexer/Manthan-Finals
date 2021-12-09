88ceb566b2487f5de1d01e9eed72794d198f3b60
gamingflexer <osurve20ece@student.mes.ac.in>
2021-12-09 11:03:13 +0530

    s

--- ML/Twint-Script/intialization.py
+++ ML/Twint-Script/intialization.py
@@ -1,6 +1,6 @@
 import twint
 
-ky=["ISIS","Khalistan","WadhawaSinghBabbar","khalistan","Airportbombing","Khalistanmovement","Inter-ServicesIntelligence","Khalistanisympathisers"]
+#ky=["ISIS","Khalistan","WadhawaSinghBabbar","khalistan","Airportbombing","Khalistanmovement","Inter-ServicesIntelligence","Khalistanisympathisers"]
 
 
 c = twint.Config()
@@ -8,11 +8,11 @@ c = twint.Config()
 #for k in ky:
 
 c.Search = "ISIS"
-c.Limit= 300
+c.Limit= 100
 c.Min_likes = 3
-c.Custom["tweet"] = ["id", "username","tweet"]
-c.Output="10.json"
-c.Store_json=True
+c.Custom["tweet"] = ["username","tweet","replies_count","retweets_count",]
+c.Output="10.csv"
+#c.Store_json=True
 
 c.Hide_output = True
 
